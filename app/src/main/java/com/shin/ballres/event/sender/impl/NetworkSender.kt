eb15da37f28a01b5b80f6ef815551ba475457075
shin <16229439+shin11788@user.noreply.gitee.com>
2026-03-23 12:54:08 +0800

    新增ReadME

--- app/src/main/java/com/shin/ballres/event/sender/impl/NetworkSender.kt
+++ app/src/main/java/com/shin/ballres/event/sender/impl/NetworkSender.kt
@@ -2,6 +2,10 @@ package com.shin.ballres.event.sender.impl
 
 import com.shin.ballres.event.Event
 import com.shin.ballres.event.sender.EventSender
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class NetworkSender : EventSender {
     override fun send(event: Event) {
@@ -9,11 +13,12 @@ class NetworkSender : EventSender {
         simulateNetworkRequest(event)
     }
 
-    //应使用Retrofit挂起替代
+    //应使用Retrofit替代,为了简单,我先这么做了,delay也封装好了协程
     private fun simulateNetworkRequest(event: Event) {
-        Thread {
-            Thread.sleep(100)
+        val scope = CoroutineScope(Dispatchers.IO)
+        scope.launch {
+            delay(1000)
             println("[Network] Response: OK for event ${event.name}")
-        }.start()
+        }
     }
 }
